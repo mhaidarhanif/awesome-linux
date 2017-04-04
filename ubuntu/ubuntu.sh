8a2c7a186f8877f861f9be51c88f0b3fd6dd531f
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-04 13:36:23 +0700

    Extra dev + ops: Docker

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -202,6 +202,18 @@ sudo apt-get install -y inkscape gimp
 sudo apt-get install -y imagemagick
 
 # ------------------------------------------------------------------------------
+# extra development + operation
+
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+# ------------------------------------------------------------------------------
 # customization
 
 # desktop environment
