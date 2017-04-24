5cba5dea94d437dc89982941297e990756a9c4d6
M Haidar Hanif <git@mhaidarhanif.com>
2017-04-24 22:58:23 +0700

    Add Google Cloud Platform section

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -338,6 +338,24 @@ sudo apt-get install -y fonts-emojione-svginot
 # sudo apt-get install -y fonts-twemoji-svginot
 
 # ------------------------------------------------------------------------------
+# google cloud
+
+# Create an environment variable for the correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk
+
+# Authenticate an account
+gcloud auth login
+
+# ------------------------------------------------------------------------------
 # cleanup
 
 sudo apt-get autoremove -y
